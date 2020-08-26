bdaff266569d4ef33dab62ae6343d5c39e64d112
bigory <igorboldysh@gmail.com>
2020-08-26 11:45:43 +0300

    update project

--- bottutall.py
+++ bottutall.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('1392723978:AAFnckkjU61gWjePx3e2gu9ZkXcczSTQdpg')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет,ты написал старт')
+    bot.send_message(message.chat.id, 'Привет,ты написал мне /start')
 
 
 bot.polling()
