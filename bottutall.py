047b89095acff6d43dd0402f4ed7e65fa65203f9
bigory <igorboldysh@gmail.com>
2020-09-18 11:18:36 +0300

    send gif_file

--- bottutall.py
+++ bottutall.py
@@ -3,10 +3,6 @@ import telebot
 bot = telebot.TeleBot('1392723978:AAFnckkjU61gWjePx3e2gu9ZkXcczSTQdpg')
 
 
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Привет,ты написал мне /start')
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет,ты написал мне /start', reply_markup=keyboard)
@@ -23,7 +19,7 @@ def send_text(message):
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай друг')
     elif message.text.lower() == 'ублюдок':
-        bot.send_document(message.chat.id, 'https://static.life.ru/publications/2020/7/6/665237996492.8094.gif')
+        bot.send_document(message.chat.id, 'https://media-exp1.licdn.com/dms/image/C4D03AQG-dPUFbiXz4A/profile-displayphoto-shrink_800_800/0?e=1605744000&v=beta&t=mVp9HSKZPGAMn5EZGeEb-QIleealUUzeHponcONyeIs')
 
 
 keyboard = telebot.types.ReplyKeyboardMarkup()
